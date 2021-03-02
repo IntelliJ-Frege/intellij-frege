ea87cd60fd1a6b5a2b07525bf01f64f8721bf026
Kirill <bitikit@gmail.com>
2021-03-02 21:38:31 +0300

    add primitive syntax highlighting

---
+++ src/main/java/com/plugin/frege/highlighter/FregeSyntaxHighlighterFactory.java
@@ -0,0 +1,16 @@
+package com.plugin.frege.highlighter;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class FregeSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new FregeSyntaxHighlighter();
+    }
+}
