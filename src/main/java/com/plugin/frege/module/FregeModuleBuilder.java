46b19082bf6e4589dd055fe0950c78016af7d8f9
Худяков Юрий <catchitoo@mail.ru>
2021-03-03 13:30:20 +0300

    Add module, framework and configuration extensions

---
+++ src/main/java/com/plugin/frege/module/FregeModuleBuilder.java
@@ -0,0 +1,37 @@
+package com.plugin.frege.module;
+
+import com.intellij.ide.util.projectWizard.ModuleBuilder;
+import com.intellij.ide.util.projectWizard.ModuleWizardStep;
+import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class FregeModuleBuilder extends ModuleBuilder {
+    public void setupRootModel(ModifiableRootModel modifiableRootModel) throws ConfigurationException {
+
+    }
+
+    public ModuleType getModuleType() {
+        return FregeModuleType.getInstance();
+    }
+
+    @Override
+    public ModuleWizardStep[] createWizardSteps(@NotNull WizardContext wizardContext, @NotNull ModulesProvider modulesProvider) {
+        return new ModuleWizardStep[]{new ModuleWizardStep() {
+            @Override
+            public JComponent getComponent() {
+                return new JLabel("Put your content here");
+            }
+
+            @Override
+            public void updateDataModel() {
+
+            }
+        }};
+    }
+}
---
+++ src/main/java/com/plugin/frege/runConfiguration/FregeRunConfigurationFactory.java
@@ -0,0 +1,24 @@
+package com.plugin.frege.runConfiguration;
+
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.project.Project;
+
+public class FregeRunConfigurationFactory extends ConfigurationFactory {
+    private static final String FACTORY_NAME = "Frege configuration factory";
+
+    protected FregeRunConfigurationFactory(ConfigurationType type) {
+        super(type);
+    }
+
+    @Override
+    public RunConfiguration createTemplateConfiguration(Project project) {
+        return new FregeRunConfiguration(project, this, "Frege");
+    }
+
+    @Override
+    public String getName() {
+        return FACTORY_NAME;
+    }
+}
