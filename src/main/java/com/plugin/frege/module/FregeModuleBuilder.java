28bb8426b577c8b6c873833f6b63c8c81a9c62d1
Худяков Юрий <catchitoo@mail.ru>
2021-04-19 03:49:47 +0300

    Figure out how to add SettingsField, add field for Frege compiler (yet without any backend)

--- src/main/java/com/plugin/frege/module/FregeModuleBuilder.java
+++ src/main/java/com/plugin/frege/module/FregeModuleBuilder.java
@@ -2,18 +2,27 @@ package com.plugin.frege.module;
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
+import com.intellij.ide.util.projectWizard.SettingsStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
+import org.jetbrains.annotations.Nullable;
 
 public class FregeModuleBuilder extends ModuleBuilder {
     public void setupRootModel(ModifiableRootModel modifiableRootModel) throws ConfigurationException {
 
+    }
+
+    @Override
+    public @Nullable ModuleWizardStep modifySettingsStep(@NotNull SettingsStep settingsStep) {
+        TextFieldWithBrowseButton selectCompilerField = new TextFieldWithBrowseButton();
+        settingsStep.addSettingsField("Frege compiler", selectCompilerField);
+
+        return super.modifySettingsStep(settingsStep);
     }
 
     public ModuleType getModuleType() {
@@ -22,16 +31,6 @@ public class FregeModuleBuilder extends ModuleBuilder {
 
     @Override
     public ModuleWizardStep[] createWizardSteps(@NotNull WizardContext wizardContext, @NotNull ModulesProvider modulesProvider) {
-        return new ModuleWizardStep[]{new ModuleWizardStep() {
-            @Override
-            public JComponent getComponent() {
-                return new JLabel("Put your content here");
-            }
-
-            @Override
-            public void updateDataModel() {
-
-            }
-        }};
+        return ModuleWizardStep.EMPTY_ARRAY;
     }
 }
