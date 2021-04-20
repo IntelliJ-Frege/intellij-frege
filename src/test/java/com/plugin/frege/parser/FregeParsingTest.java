6ca2f8580d61e35b89d8f045ddbd7d65787f8e65
Petya Surkov <petyo333@gmail.com>
2021-04-20 15:20:23 +0300

    Add frege parser tests

---
+++ src/test/java/com/plugin/frege/parser/FregeParsingTest.java
@@ -0,0 +1,48 @@
+package com.plugin.frege.parser;
+
+import com.intellij.testFramework.ParsingTestCase;
+
+public class FregeParsingTest extends ParsingTestCase {
+    public FregeParsingTest() {
+        super("", "fr", new FregeParserDefinition());
+    }
+
+    public void testClassDcl() {
+        doTest(true);
+    }
+
+    public void testDataDcl() {
+        doTest(true);
+    }
+
+    public void testImports() {
+        doTest(true);
+    }
+
+    public void testInstanceDcl() {
+        doTest(true);
+    }
+
+    public void testSimpleProgram() {
+        doTest(true);
+    }
+
+    public void testTypes() {
+        doTest(true);
+    }
+
+    @Override
+    protected boolean skipSpaces() {
+        return false;
+    }
+
+    @Override
+    protected boolean includeRanges() {
+        return true;
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData/psi";
+    }
+}
