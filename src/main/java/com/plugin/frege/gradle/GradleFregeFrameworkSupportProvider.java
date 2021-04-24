fac83c5ab9846aa5ffd28fa579332fdcc4c5952a
Худяков Юрий <catchitoo@mail.ru>
2021-04-24 16:30:21 +0300

    Add WIP files for gradle support

---
+++ src/main/java/com/plugin/frege/gradle/GradleFregeFrameworkSupportProvider.java
@@ -0,0 +1,6 @@
+package com.plugin.frege.gradle;
+
+import org.jetbrains.plugins.gradle.frameworkSupport.GradleFrameworkSupportProvider;
+
+public class GradleFregeFrameworkSupportProvider extends GradleFrameworkSupportProvider {
+}
