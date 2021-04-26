65839e358f2183642cf830806d84126dbea0dacb
Kirill <bitikit@gmail.com>
2021-04-27 01:47:35 +0300

    Apply psi util methods for resolving references

--- src/main/java/com/plugin/frege/resolve/FregeFunctionNameReference.java
+++ src/main/java/com/plugin/frege/resolve/FregeFunctionNameReference.java
@@ -3,29 +3,32 @@ package com.plugin.frege.resolve;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.plugin.frege.psi.FregeBody;
+import com.plugin.frege.psi.FregeDecl;
 import com.plugin.frege.psi.FregeFunctionName;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
+
+import static com.plugin.frege.psi.impl.FregePsiUtilImpl.*;
 
 public class FregeFunctionNameReference extends FregeReferenceBase {
     public FregeFunctionNameReference(@NotNull PsiElement element) {
         super(element, new TextRange(0, element.getTextLength()));
     }
 
-    // TODO take into account scopes
+    // TODO improve
     @Override
     protected List<PsiElement> resolveInner(boolean incompleteCode) {
-        FregeBody body = PsiTreeUtil.getParentOfType(element, FregeBody.class);
-        if (body == null) {
+        PsiElement scope = scopeOfElement(element);
+        if (scope == null) {
             return List.of();
         }
-        return PsiTreeUtil.findChildrenOfType(body, FregeFunctionName.class).stream()
-                .filter(Objects::nonNull)
-                .filter(elem -> Objects.equals(elem.getText(), element.getText()))
+
+        String referenceText = element.getText();
+        return declsFromScopeOfElement(element, FregeDecl::getBinding).stream()
+                .map(binding -> PsiTreeUtil.findChildOfType(binding, FregeFunctionName.class))
+                .filter(keepWithText(referenceText))
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/com/plugin/frege/resolve/FregeQVaridReference.java
+++ src/main/java/com/plugin/frege/resolve/FregeQVaridReference.java
@@ -3,28 +3,53 @@ package com.plugin.frege.resolve;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.plugin.frege.psi.*;
+import com.plugin.frege.psi.FregeDecl;
+import com.plugin.frege.psi.FregeFunctionName;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
+
+import static com.plugin.frege.psi.impl.FregePsiUtilImpl.*;
 
 public class FregeQVaridReference extends FregeReferenceBase {
     public FregeQVaridReference(@NotNull PsiElement element) {
         super(element, new TextRange(0, element.getTextLength()));
     }
 
-    // TODO take into account scopes and qualified names
+    // TODO take into account: qualified names and parameters of bindings.
     @Override
     protected List<PsiElement> resolveInner(boolean incompleteCode) {
-        FregeBody body = PsiTreeUtil.getParentOfType(element, FregeBody.class);
-        if (body == null) {
-            return List.of();
+        String referenceText = element.getText();
+
+        // check if this expression has `where` ans search there for definitions if it does.
+        PsiElement whereDecls = findWhereDeclsInExpression(element);
+        if (whereDecls != null) {
+            List<PsiElement> whereFuncNames = declsFromScopeOfElement(whereDecls, FregeDecl::getBinding).stream()
+                    .map(binding -> PsiTreeUtil.findChildOfType(binding, FregeFunctionName.class))
+                    .filter(keepWithText(referenceText))
+                    .collect(Collectors.toList());
+
+            if (!whereFuncNames.isEmpty()) {
+                return whereFuncNames;
+            }
         }
-        return PsiTreeUtil.findChildrenOfType(body, FregeFunctionName.class).stream()
-                .filter(Objects::nonNull)
-                .filter(funcName -> Objects.equals(funcName.getName(), element.getText()))
-                .collect(Collectors.toList());
+
+        // searching for definitions in the current and outer scopes
+        PsiElement scope = scopeOfElement(element);
+        while (scope != null) {
+            List<PsiElement> functionNames = declsFromScopeOfElement(scope, FregeDecl::getBinding).stream()
+                    .map(binding -> PsiTreeUtil.findChildOfType(binding, FregeFunctionName.class))
+                    .filter(keepWithText(referenceText))
+                    .collect(Collectors.toList());
+
+            if (!functionNames.isEmpty()) {
+                return functionNames;
+            }
+
+            scope = scopeOfElement(scope.getParent());
+        }
+
+        return List.of();
     }
 }
