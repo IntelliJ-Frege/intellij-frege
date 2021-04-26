452cb73558c7d483244eaa41112e0a860e073d09
Kirill <bitikit@gmail.com>
2021-04-26 21:31:18 +0300

    Update function name reference

--- src/main/java/com/plugin/frege/psi/mixin/FregeFunctionNameMixin.java
+++ src/main/java/com/plugin/frege/psi/mixin/FregeFunctionNameMixin.java
@@ -2,7 +2,6 @@ package com.plugin.frege.psi.mixin;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiNameIdentifierOwner;
 import com.intellij.psi.PsiReference;
 import com.intellij.util.IncorrectOperationException;
 import com.plugin.frege.psi.FregeElementFactory;
@@ -10,12 +9,15 @@ import com.plugin.frege.psi.FregeFunctionName;
 import com.plugin.frege.psi.impl.FregeNamedElementImpl;
 import com.plugin.frege.resolve.FregeFunctionNameReference;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public abstract class FregeFunctionNameMixin extends FregeNamedElementImpl implements FregeFunctionName, PsiNameIdentifierOwner, PsiElement {
-
+public abstract class FregeFunctionNameMixin extends FregeNamedElementImpl implements FregeFunctionName {
     public FregeFunctionNameMixin(@NotNull ASTNode node) {
         super(node);
+    }
+
+    @Override
+    public @NotNull PsiElement getNameIdentifier() {
+        return this;
     }
 
     @Override
@@ -27,15 +29,5 @@ public abstract class FregeFunctionNameMixin extends FregeNamedElementImpl imple
     @Override
     public PsiReference getReference() {
         return new FregeFunctionNameReference(this);
-    }
-
-    @Override
-    public String getName() {
-        return getNameIdentifier().getText();
-    }
-
-    @Override
-    public @Nullable PsiElement getNameIdentifier() {
-        return this;
     }
 }
--- src/main/java/com/plugin/frege/resolve/FregeFunctionNameReference.java
+++ src/main/java/com/plugin/frege/resolve/FregeFunctionNameReference.java
@@ -1,17 +1,31 @@
 package com.plugin.frege.resolve;
 
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.plugin.frege.psi.FregeBody;
+import com.plugin.frege.psi.FregeFunctionName;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class FregeFunctionNameReference extends FregeReferenceBase {
     public FregeFunctionNameReference(@NotNull PsiElement element) {
-        super(element);
+        super(element, new TextRange(0, element.getTextLength()));
     }
 
+    // TODO take into account scopes
     @Override
     protected List<PsiElement> resolveInner(boolean incompleteCode) {
-        return List.of(); // TODO
+        FregeBody body = PsiTreeUtil.getParentOfType(element, FregeBody.class);
+        if (body == null) {
+            return List.of();
+        }
+        return PsiTreeUtil.findChildrenOfType(body, FregeFunctionName.class).stream()
+                .filter(Objects::nonNull)
+                .filter(elem -> Objects.equals(elem.getText(), element.getText()))
+                .collect(Collectors.toList());
     }
 }
