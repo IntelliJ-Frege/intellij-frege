be9052bfae139604c3b365444f59b777f4dd0a17
Kirill <bitikit@gmail.com>
2021-04-26 21:29:44 +0300

    Annotate methods of elements factory with notnull

--- src/main/java/com/plugin/frege/psi/FregeElementFactory.java
+++ src/main/java/com/plugin/frege/psi/FregeElementFactory.java
@@ -5,24 +5,25 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.plugin.frege.FregeLanguage;
+import org.jetbrains.annotations.NotNull;
 
 public class FregeElementFactory {
     private static final String fakeModuleName = "Dummy";
     private static final String fakeFileName = fakeModuleName + ".fr";
     private static final String fakeProgram = "module " + fakeModuleName + " where\n";
 
-    private static FregeFile createFile(Project project, String text) {
+    private static @NotNull FregeFile createFile(Project project, String text) {
         return (FregeFile) PsiFileFactory.getInstance(project).createFileFromText(fakeFileName, FregeLanguage.INSTANCE, text);
     }
 
-    private static <E extends PsiElement> E createElement(Project project, String text, Class<E> elementClass) {
+    private static <E extends PsiElement> @NotNull E createElement(Project project, String text, Class<E> elementClass) {
         FregeFile file = createFile(project, text);
         return PsiTreeUtil.findChildrenOfType(file, elementClass).stream()
                 .findFirst().orElseThrow(() ->
                         new IllegalStateException("Cannot create a function with a custom name."));
     }
 
-    public static FregeFunctionName createFunctionName(Project project, String name) {
+    public static @NotNull FregeFunctionName createFunctionName(Project project, String name) {
         String fakeFunction = fakeProgram + name + " = undefined";
         return createElement(project, fakeFunction, FregeFunctionName.class);
     }
