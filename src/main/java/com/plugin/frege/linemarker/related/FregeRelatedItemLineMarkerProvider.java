f10cbc31cbd6ed2b2fb5c3b93689496393c8ac2f
Petya Surkov <petyo333@gmail.com>
2021-05-03 14:28:02 +0300

    Implement line marker provider to navigate from function binding to annotation (if exists)

---
+++ src/main/java/com/plugin/frege/linemarker/related/FregeRelatedItemLineMarkerProvider.java
@@ -0,0 +1,17 @@
+package com.plugin.frege.linemarker.related;
+
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+public class FregeRelatedItemLineMarkerProvider extends RelatedItemLineMarkerProvider {
+    FregeFunctionBindingToAnnotationLineMarker functionBindingToAnnotation = new FregeFunctionBindingToAnnotationLineMarker();
+
+    @Override
+    protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
+        functionBindingToAnnotation.addMarker(element, result);
+    }
+}
