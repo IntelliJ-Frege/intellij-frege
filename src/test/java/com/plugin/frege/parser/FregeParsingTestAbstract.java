6649b9654c59f659a5cdb33e815e1ef4def4b13b
Petya Surkov <petyo333@gmail.com>
2021-05-04 21:04:28 +0300

    Improve parsing tests system to create successfully parsing tests, fail parsing tests, and accurate tests with reference psi tree. Add Frege examples programs from its repository and test parser on it.

---
+++ src/test/java/com/plugin/frege/parser/FregeParsingTestAbstract.java
@@ -0,0 +1,42 @@
+package com.plugin.frege.parser;
+
+import com.intellij.testFramework.ParsingTestCase;
+import junit.framework.ComparisonFailure;
+
+import java.io.IOException;
+
+public abstract class FregeParsingTestAbstract extends ParsingTestCase {
+    public FregeParsingTestAbstract() {
+        super("", "fr", new FregeParserDefinition());
+    }
+
+    protected void doSuccessfulParsingTest() throws IOException {
+        String name = getTestName();
+        parseFile(name, loadFile(name + "." + myFileExt));
+        ensureNoErrorElements();
+    }
+
+    protected void doFailParsingTest() throws IOException {
+        boolean parsingFail = false;
+        try {
+            doSuccessfulParsingTest();
+        } catch (ComparisonFailure ignored) {
+            parsingFail = true;
+        }
+        if (!parsingFail) {
+            fail("Successful parse file " + myFile.getName());
+        }
+    }
+
+    protected void doAccurateTest() throws IOException {
+        doTest(true);
+    }
+
+    @Override
+    protected boolean includeRanges() {
+        return true;
+    }
+
+    @Override
+    protected abstract String getTestDataPath();
+}
---
+++ src/test/java/com/plugin/frege/parser/FregeSuccessfulParsingValidProgramsTest.java
@@ -0,0 +1,43 @@
+package com.plugin.frege.parser;
+
+import java.io.IOException;
+
+public class FregeSuccessfulParsingValidProgramsTest extends FregeParsingTestAbstract {
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData/parsing/validPrograms";
+    }
+
+    public void testClassDcl() throws IOException {
+        doSuccessfulParsingTest();
+    }
+
+    public void testDataDcl() throws IOException {
+        doSuccessfulParsingTest();
+    }
+
+    public void testImports() throws IOException {
+        doSuccessfulParsingTest();
+    }
+
+    public void testInstanceDcl() throws IOException {
+        doSuccessfulParsingTest();
+    }
+
+    public void testSimpleProgram() throws IOException {
+        doSuccessfulParsingTest();
+    }
+
+    public void testTypes() throws IOException {
+        doSuccessfulParsingTest();
+    }
+
+    public void testLayoutRule() throws IOException {
+        doSuccessfulParsingTest();
+    }
+
+    public void testList() throws IOException {
+        doSuccessfulParsingTest();
+    }
+}
