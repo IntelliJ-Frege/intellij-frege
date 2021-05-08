f497dd44dcd41faf75c9b5d20cb3200acb1f7b6b
Petya Surkov <petyo333@gmail.com>
2021-05-09 00:16:17 +0300

    Add data record tests, fix case expression tests, add one more test for patterns.

--- src/test/java/com/plugin/frege/parser/FregeSuccessfulParsingValidProgramsTest.java
+++ src/test/java/com/plugin/frege/parser/FregeSuccessfulParsingValidProgramsTest.java
@@ -52,4 +52,8 @@ public class FregeSuccessfulParsingValidProgramsTest extends FregeParsingTestAbs
     public void testCaseEx() throws IOException {
         doSuccessfulParsingTest();
     }
+
+    public void testDataRecord() throws IOException {
+        doSuccessfulParsingTest();
+    }
 }
