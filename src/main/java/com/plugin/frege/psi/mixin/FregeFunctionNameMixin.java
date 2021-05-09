ae10be08ab4a26dc5b99537c66c38b94c8998f58
Kirill <bitikit@gmail.com>
2021-05-10 00:00:56 +0300

    Implement frege psi method and make function name extend it

--- src/main/java/com/plugin/frege/psi/mixin/FregeFunctionNameMixin.java
+++ src/main/java/com/plugin/frege/psi/mixin/FregeFunctionNameMixin.java
@@ -2,34 +2,77 @@ package com.plugin.frege.psi.mixin;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.NlsSafe;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.light.LightModifierList;
+import com.intellij.psi.impl.source.HierarchicalMethodSignatureImpl;
+import com.intellij.psi.impl.source.tree.java.PsiCodeBlockImpl;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.MethodSignatureBackedByPsiMethod;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
-import com.plugin.frege.psi.FregeAnnoItem;
-import com.plugin.frege.psi.FregeElementFactory;
-import com.plugin.frege.psi.FregeFunLhs;
-import com.plugin.frege.psi.FregeLhs;
-import com.plugin.frege.psi.impl.FregeNamedElementImpl;
+import com.plugin.frege.FregeLanguage;
+import com.plugin.frege.psi.*;
+import com.plugin.frege.psi.impl.FregePsiMethodImpl;
 import com.plugin.frege.psi.impl.FregePsiUtilImpl;
 import com.plugin.frege.resolve.FregeFunctionNameReference;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
-public abstract class FregeFunctionNameMixin extends FregeNamedElementImpl {
+import static com.plugin.frege.psi.FregeTypes.FUNCTION_NAME;
+
+@SuppressWarnings("UnstableApiUsage")
+public class FregeFunctionNameMixin extends FregePsiMethodImpl implements PsiIdentifier {
+
+    private final LightModifierList modifierList;
+
     public FregeFunctionNameMixin(@NotNull ASTNode node) {
         super(node);
+        modifierList = new LightModifierList(getManager(), FregeLanguage.INSTANCE,
+                PsiModifier.STATIC, PsiModifier.FINAL, PsiModifier.PUBLIC); // TODO
     }
 
     @Override
-    public @NotNull PsiElement getNameIdentifier() {
+    public @NotNull PsiType getReturnType() {
+        return objectType;
+    }
+
+    @Override
+    public @Nullable PsiTypeElement getReturnTypeElement() {
+        return typeElement;
+    }
+
+    @Override
+    public @NotNull PsiCodeBlock getBody() {
+        FregeBinding binding = PsiTreeUtil.getParentOfType(this, FregeBinding.class);
+        String text = binding == null ? getText() : binding.getText(); // TODO
+        return new PsiCodeBlockImpl(text);
+    }
+
+    @Override
+    public boolean isConstructor() {
+        return false;
+    }
+
+    @Override
+    public @NotNull PsiIdentifier getNameIdentifier() {
         return this;
+    }
+
+    @Override
+    public @NotNull PsiModifierList getModifierList() {
+        return modifierList;
     }
 
     @Override
     public PsiElement setName(@NlsSafe @NotNull String name) throws IncorrectOperationException {
         return getNameIdentifier().replace(FregeElementFactory.createFunctionName(getProject(), name));
+    }
+
+    @Override
+    public @NotNull HierarchicalMethodSignature getHierarchicalMethodSignature() {
+        return new HierarchicalMethodSignatureImpl(MethodSignatureBackedByPsiMethod.create(this, PsiSubstitutor.EMPTY));
     }
 
     @Override
@@ -45,19 +88,26 @@ public abstract class FregeFunctionNameMixin extends FregeNamedElementImpl {
         return getParent() instanceof FregeAnnoItem;
     }
 
-    private int getArgsCount() {
+    @Override
+    protected int getParamsNumber() {
         if (!isFunctionBinding()) {
             return 0; // TODO
         }
-        return Objects.requireNonNull(
-                PsiTreeUtil.findFirstParent(this, p -> p instanceof FregeLhs)).getChildren().length; // TODO support operators and patterns
+
+        PsiElement fregeLhs = Objects.requireNonNull(PsiTreeUtil.getParentOfType(this, FregeLhs.class));
+        return PsiTreeUtil.findChildrenOfType(fregeLhs, FregeParam.class).size();
     }
 
     public boolean isMainFunctionBinding() {
-        int argsCount = getArgsCount();
+        int argsCount = getParamsNumber();
         return isFunctionBinding()
                 && argsCount <= 1
                 && FregePsiUtilImpl.isInGlobalScope(this)
                 && getText().equals("main");
+    }
+
+    @Override
+    public IElementType getTokenType() {
+        return FUNCTION_NAME;
     }
 }
