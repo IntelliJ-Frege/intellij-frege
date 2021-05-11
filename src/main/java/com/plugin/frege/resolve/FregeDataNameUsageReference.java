2432df50cf003e9b10a5a5f0bdca2eb14e4cae3a
Kirill <bitikit@gmail.com>
2021-05-12 02:17:13 +0300

    Implement resolving native data

--- src/main/java/com/plugin/frege/resolve/FregeDataNameUsageReference.java
+++ src/main/java/com/plugin/frege/resolve/FregeDataNameUsageReference.java
@@ -1,13 +1,18 @@
 package com.plugin.frege.resolve;
 
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
-import com.plugin.frege.psi.FregeDataName;
+import com.plugin.frege.psi.FregeDataNameNative;
 import com.plugin.frege.psi.FregeElementFactory;
+import com.plugin.frege.psi.impl.FregePsiClassUtilImpl;
+import com.plugin.frege.psi.impl.FregePsiUtilImpl;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -22,16 +27,41 @@ public class FregeDataNameUsageReference extends FregeReferenceBase {
 
     @Override
     protected List<PsiElement> resolveInner(boolean incompleteCode) {
-        String referenceText = element.getText();
-        return findAvailableDataDecls(element).stream()
-                .map(decl -> PsiTreeUtil.findChildOfType(decl, FregeDataName.class))
-                .map(Objects::requireNonNull)
-                .filter(keepWithText(referenceText))
-                .collect(Collectors.toList());
+        List<PsiElement> currentFileData = tryFindDataInCurrentFile();
+        if (!currentFileData.isEmpty()) {
+            return currentFileData;
+        }
+
+        return tryFindDataByImports();
     }
 
     @Override
     public PsiElement handleElementRename(@NotNull String name) throws IncorrectOperationException {
         return element.replace(FregeElementFactory.createDataNameUsage(element.getProject(), name));
+    }
+
+    private List<PsiElement> tryFindDataInCurrentFile() {
+        String referenceText = element.getText();
+        return findAvailableDataDecls(element).stream()
+                .map(decl -> PsiTreeUtil.findChildOfType(decl, FregeDataNameNative.class))
+                .map(Objects::requireNonNull)
+                .filter(keepWithText(referenceText))
+                .collect(Collectors.toList());
+    }
+
+    private List<PsiElement> tryFindDataByImports() {
+        String clazzName = element.getText();
+        Project project = element.getProject();
+
+        List<String> imports = FregePsiUtilImpl.findImportsNamesForElement(element, true);
+        for (String currentImport : imports) {
+            String qualifiedName = currentImport + "." + clazzName;
+            List<PsiClass> classes = FregePsiClassUtilImpl.getClassesByQualifiedName(project, qualifiedName);
+            if (!classes.isEmpty()) {
+                return new ArrayList<>(classes);
+            }
+        }
+
+        return List.of();
     }
 }
