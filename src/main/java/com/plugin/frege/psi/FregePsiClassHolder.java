0ca5511611aff6ce96943b572f37d5ba9adfe37a
Kirill <bitikit@gmail.com>
2021-05-14 00:34:12 +0300

    Fix NPE in navigation to classes and methods

--- src/main/java/com/plugin/frege/psi/FregePsiClassHolder.java
+++ src/main/java/com/plugin/frege/psi/FregePsiClassHolder.java
@@ -1,14 +1,13 @@
 package com.plugin.frege.psi;
 
-import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Indicates that it must hold a {@link FregePsiClass} in children.
  * Helps to get a containing class for an element.
  */
 public interface FregePsiClassHolder extends PsiElement {
-    @NotNull
+    @Nullable
     FregePsiClass getHoldingClass();
 }
