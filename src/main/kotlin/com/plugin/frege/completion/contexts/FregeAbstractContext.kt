cf066f24942e953c3b86971751c985ab0799e16b
Petya Surkov <petyo333@gmail.com>
2021-05-18 15:24:09 +0300

    Create if-then-else live template

---
+++ src/main/kotlin/com/plugin/frege/completion/contexts/FregeAbstractContext.kt
@@ -0,0 +1,13 @@
+package com.plugin.frege.completion.contexts
+
+import com.intellij.codeInsight.template.TemplateContextType
+import com.intellij.psi.PsiFile
+
+abstract class FregeAbstractContext(idSuffix: String, presentableName: String) :
+    TemplateContextType("FREGE_$idSuffix", presentableName, Generic::class.java) {
+    class Generic : TemplateContextType("FREGE", "Frege") {
+        override fun isInContext(file: PsiFile, offset: Int): Boolean {
+            return true
+        }
+    }
+}
