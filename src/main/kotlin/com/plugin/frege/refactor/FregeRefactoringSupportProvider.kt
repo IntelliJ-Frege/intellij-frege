35a4c7eeef6dc478537975b2727d73413438baee
Kirill <bitikit@gmail.com>
2021-05-18 20:24:46 +0300

    Implement inplace renaming

---
+++ src/main/kotlin/com/plugin/frege/refactor/FregeRefactoringSupportProvider.kt
@@ -0,0 +1,11 @@
+package com.plugin.frege.refactor
+
+import com.intellij.lang.refactoring.RefactoringSupportProvider
+import com.intellij.psi.PsiElement
+import com.plugin.frege.psi.FregeNamedElement
+
+class FregeRefactoringSupportProvider : RefactoringSupportProvider() {
+    override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
+        return element is FregeNamedElement
+    }
+}
