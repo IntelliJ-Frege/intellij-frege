e51f8a21b86a03a004a89c478e406a8e0b0668bb
Petya Surkov <petyo333@gmail.com>
2021-05-18 15:24:09 +0300

    Create if-then-else live template

---
+++ src/main/kotlin/com/plugin/frege/completion/contexts/FregePatternedContext.kt
@@ -0,0 +1,24 @@
+package com.plugin.frege.completion.contexts
+
+import com.intellij.patterns.PsiElementPattern
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.plugin.frege.FregeFileType
+import com.plugin.frege.completion.patterns.CondPatterns
+
+abstract class FregePatternedContext(idSuffix: String, presentableName: String) :
+    FregeAbstractContext(idSuffix, presentableName) {
+    abstract val pattern: PsiElementPattern.Capture<PsiElement>
+
+    override fun isInContext(file: PsiFile, offset: Int): Boolean {
+        if (file.fileType != FregeFileType.INSTANCE) {
+            return false
+        }
+        return pattern.accepts(file.findElementAt(offset))
+    }
+
+    class FregeIf : FregePatternedContext("IF", "If") {
+        override val pattern: PsiElementPattern.Capture<PsiElement>
+            get() = CondPatterns.ifPattern()
+    }
+}
