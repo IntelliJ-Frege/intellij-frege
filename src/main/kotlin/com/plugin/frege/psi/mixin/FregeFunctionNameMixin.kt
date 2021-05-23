936a675c0158a444cf29c1b6d4e7bf79b58f95a9
Kirill <bitikit@gmail.com>
2021-05-23 22:08:03 +0300

    Fix operators navigation

--- src/main/kotlin/com/plugin/frege/psi/mixin/FregeFunctionNameMixin.kt
+++ src/main/kotlin/com/plugin/frege/psi/mixin/FregeFunctionNameMixin.kt
@@ -6,23 +6,17 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiReference
 import com.intellij.psi.tree.IElementType
-import com.plugin.frege.psi.FregeBinding
 import com.plugin.frege.psi.FregeElementFactory
 import com.plugin.frege.psi.FregeTypes
 import com.plugin.frege.psi.impl.FregeCompositeElementImpl
-import com.plugin.frege.psi.impl.FregePsiUtilImpl
 import com.plugin.frege.resolve.FregeReferenceBase
+import com.plugin.frege.resolve.FregeResolveUtil.resolveBindingByNameElement
 
 open class FregeFunctionNameMixin(node: ASTNode) : FregeCompositeElementImpl(node), PsiIdentifier {
     override fun getReference(): PsiReference {
         return object : FregeReferenceBase(this, TextRange(0, textLength)) {
             override fun resolveInner(incompleteCode: Boolean): List<PsiElement> {
-                val firstBinding = FregePsiUtilImpl.findElementsWithinScope(
-                    psiElement,
-                    FregePsiUtilImpl.getByTypePredicateCheckingName(FregeBinding::class, psiElement.text, incompleteCode)
-                ).minByOrNull { it.textOffset }
-
-                return if (firstBinding != null) listOf(firstBinding) else emptyList()
+                return resolveBindingByNameElement(psiElement, incompleteCode)
             }
 
             override fun handleElementRename(name: String): PsiElement {
--- src/main/kotlin/com/plugin/frege/psi/mixin/FregeSymopMixin.kt
+++ src/main/kotlin/com/plugin/frege/psi/mixin/FregeSymopMixin.kt
@@ -6,16 +6,27 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiReference
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.util.parentOfTypes
+import com.plugin.frege.psi.FregeAnnoItem
+import com.plugin.frege.psi.FregeFunLhs
 import com.plugin.frege.psi.FregeTypes
 import com.plugin.frege.psi.impl.FregeCompositeElementImpl
 import com.plugin.frege.resolve.FregeReferenceBase
-import com.plugin.frege.resolve.FregeResolveUtil
+import com.plugin.frege.resolve.FregeResolveUtil.findMethodsFromUsage
+import com.plugin.frege.resolve.FregeResolveUtil.resolveBindingByNameElement
 
 open class FregeSymopMixin(node: ASTNode) : FregeCompositeElementImpl(node), PsiIdentifier {
     override fun getReference(): PsiReference {
         return object : FregeReferenceBase(this, TextRange(0, textLength)) {
             override fun resolveInner(incompleteCode: Boolean): List<PsiElement> {
-                return FregeResolveUtil.findMethodsFromUsage(psiElement, incompleteCode)
+                return when {
+                    psiElement.parentOfTypes(FregeAnnoItem::class, FregeFunLhs::class) != null -> {
+                        resolveBindingByNameElement(psiElement, incompleteCode)
+                    }
+                    else -> {
+                        findMethodsFromUsage(psiElement, incompleteCode)
+                    }
+                }
             }
         }
     }
