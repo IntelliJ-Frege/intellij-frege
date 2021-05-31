9ebff9b9748992daf901bce2c0aab5d660f04be7
Kirill <bitikit@gmail.com>
2021-05-31 22:47:14 +0300

    Fix displaying of stub-based elements in psi tree

--- src/main/kotlin/com/plugin/frege/psi/impl/FregeNamedStubBasedPsiElementBase.kt
+++ src/main/kotlin/com/plugin/frege/psi/impl/FregeNamedStubBasedPsiElementBase.kt
@@ -1,12 +1,12 @@
 package com.plugin.frege.psi.impl
 
-import com.intellij.psi.stubs.StubElement
 import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
-import com.plugin.frege.psi.FregeNamedElement
 import com.intellij.psi.stubs.IStubElementType
+import com.intellij.psi.stubs.StubElement
 import com.plugin.frege.FregeIcons
+import com.plugin.frege.psi.FregeNamedElement
 import javax.swing.Icon
 
 abstract class FregeNamedStubBasedPsiElementBase<T : StubElement<*>> : StubBasedPsiElementBase<T>, FregeNamedElement {
@@ -25,6 +25,10 @@ abstract class FregeNamedStubBasedPsiElementBase<T : StubElement<*>> : StubBased
         } else {
             super.getTextOffset()
         }
+    }
+
+    override fun toString(): String {
+        return node.elementType.toString()
     }
 
     override fun getIcon(flags: Int): Icon? {
