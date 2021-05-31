8bdcbecbf11025cf64faa92018c4e3b1c6450827
Kirill <bitikit@gmail.com>
2021-05-31 20:44:57 +0300

    Implement resolving methods without class-name-qualifier

--- src/main/java/com/plugin/frege/psi/FregePsiMethod.java
+++ src/main/java/com/plugin/frege/psi/FregePsiMethod.java
@@ -1,6 +1,10 @@
 package com.plugin.frege.psi;
 
 import com.intellij.psi.PsiMethod;
+import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("UnstableApiUsage")
 public interface FregePsiMethod extends FregeNamedElement, PsiMethod {
+    @Override
+    @Nullable FregePsiClass getContainingClass();
 }
