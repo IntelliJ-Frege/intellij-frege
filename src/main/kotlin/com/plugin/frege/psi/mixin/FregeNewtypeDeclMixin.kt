8f0955977408b16d195418d83cc9b629efe5ecb9
Kirill <bitikit@gmail.com>
2021-06-01 20:14:02 +0300

    Implement support of methods in newtype

--- src/main/kotlin/com/plugin/frege/psi/mixin/FregeNewtypeDeclMixin.kt
+++ src/main/kotlin/com/plugin/frege/psi/mixin/FregeNewtypeDeclMixin.kt
@@ -5,7 +5,10 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.stubs.IStubElementType
+import com.intellij.util.containers.addIfNotNull
+import com.plugin.frege.psi.FregeDecl
 import com.plugin.frege.psi.FregeNewtypeDecl
+import com.plugin.frege.psi.FregePsiMethod
 import com.plugin.frege.psi.impl.FregePsiClassImpl
 import com.plugin.frege.stubs.FregeClassStub
 
@@ -28,8 +31,11 @@ abstract class FregeNewtypeDeclMixin : FregePsiClassImpl, FregeNewtypeDecl {
     }
 
     override fun getMethods(): Array<PsiMethod> {
-        val construct = construct
-        return if (construct != null) arrayOf(construct) else PsiMethod.EMPTY_ARRAY
+        val methods: MutableList<FregePsiMethod> = whereSection?.linearIndentSection?.subprogramsFromScope
+            ?.mapNotNull { (it as? FregeDecl)?.binding }
+            ?.toMutableList() ?: mutableListOf()
+        methods.addIfNotNull(construct)
+        return methods.toTypedArray()
     }
 
     override fun isInterface(): Boolean {
