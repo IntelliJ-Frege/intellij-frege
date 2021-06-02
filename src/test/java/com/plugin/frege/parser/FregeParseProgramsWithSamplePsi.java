b9ceba2aadbca8af7f3d6f7184faa0e3b86fd2a2
Petya Surkov <petyo333@gmail.com>
2021-06-02 13:52:02 +0300

    Fix the bug that the qualified operator in the term is parsed as primary

---
+++ src/test/java/com/plugin/frege/parser/FregeParseProgramsWithSamplePsi.java
@@ -0,0 +1,14 @@
+package com.plugin.frege.parser;
+
+import java.io.IOException;
+
+public class FregeParseProgramsWithSamplePsi extends FregeParsingTestAbstract {
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData/parsing/programsWithSamplePsi";
+    }
+
+    public void testQLexOperatorIsNotPrimary() throws IOException {
+        doAccurateTest();
+    }
+}
