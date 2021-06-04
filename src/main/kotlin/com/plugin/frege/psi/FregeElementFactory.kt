7c3d7cf8f1215f8079ee4e00175640c91c365583
Kirill <bitikit@gmail.com>
2021-06-05 02:00:02 +0300

    Refactor frege element factory

--- src/main/kotlin/com/plugin/frege/psi/FregeElementFactory.kt
+++ src/main/kotlin/com/plugin/frege/psi/FregeElementFactory.kt
@@ -5,8 +5,6 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.util.PsiTreeUtil
 import com.plugin.frege.FregeLanguage
-import java.lang.IllegalArgumentException
-import kotlin.reflect.KClass
 
 object FregeElementFactory {
     private const val fakeModuleName = "Dummy"
@@ -18,71 +16,73 @@ object FregeElementFactory {
             .createFileFromText(fakeFileName, FregeLanguage.INSTANCE, text) as FregeFile
     }
 
-    private fun <E : PsiElement> createElementOrNull(
+    private inline fun <reified E : PsiElement> createElementOrNull(
         project: Project,
         text: String,
-        elementClass: KClass<E>
     ): E? {
         val file = createFile(project, text)
-        return PsiTreeUtil.findChildrenOfType(file, elementClass.java).lastOrNull()
+        return PsiTreeUtil.findChildrenOfType(file, E::class.java).lastOrNull()
     }
 
-    private fun <E : PsiElement> createElement(project: Project, text: String, elementClass: KClass<E>): E {
-        return createElementOrNull(project, text, elementClass)
+    private inline fun <reified E : PsiElement> createElement(
+        project: Project,
+        text: String,
+    ): E {
+        return createElementOrNull(project, text)
             ?: throw IllegalArgumentException("Cannot create an element in the factory.")
     }
 
     @JvmStatic
     fun createFunctionName(project: Project, name: String): FregeFunctionName {
         val fakeFunction = "$fakeProgram$name = undefined"
-        return createElement(project, fakeFunction, FregeFunctionName::class)
+        return createElement(project, fakeFunction)
     }
 
     @JvmStatic
     fun createParam(project: Project, name: String): FregeParameter {
         val fakeParam = fakeProgram + "function " + name + " = undefined"
-        return createElement(project, fakeParam, FregeParameter::class)
+        return createElement(project, fakeParam)
     }
 
     @JvmStatic
     fun createVarId(project: Project, name: String): FregeQVarid {
         val fakeVarId = fakeProgram + "function = " + name
-        return createElement(project, fakeVarId, FregeQVarid::class)
+        return createElement(project, fakeVarId)
     }
 
     @JvmStatic
     fun createConidUsage(project: Project, name: String): FregeConidUsage {
         val fakeConidUsage = "${fakeProgram}func :: $name"
-        return createElement(project, fakeConidUsage, FregeConidUsage::class)
+        return createElement(project, fakeConidUsage)
     }
 
     @JvmStatic
     fun createAnnotationName(project: Project, name: String): FregeAnnotationName {
         val fakeAnnotationName = "${fakeProgram}$name :: Int"
-        return createElement(project, fakeAnnotationName, FregeAnnotationName::class)
+        return createElement(project, fakeAnnotationName)
     }
 
     @JvmStatic
     fun createPackageToken(project: Project, name: String): FregePackageToken {
         val fakePackageToken = "${fakeProgram}import $name.Hello\nmain = undefined"
-        return createElement(project, fakePackageToken, FregePackageToken::class)
+        return createElement(project, fakePackageToken)
     }
 
     @JvmStatic
     fun createNativeFunctionName(project: Project, name: String): FregeNativeFunctionName {
         val fakeNativeFunctionName = "${fakeProgram}native $name :: Int -> Int"
-        return createElement(project, fakeNativeFunctionName, FregeNativeFunctionName::class)
+        return createElement(project, fakeNativeFunctionName)
     }
 
     @JvmStatic
     fun createSymbolOperator(project: Project, name: String): FregeSymbolOperator {
         val fakeSymbolOperator = "${fakeProgram}($name) :: Int -> Int -> Int"
-        return createElement(project, fakeSymbolOperator, FregeSymbolOperator::class)
+        return createElement(project, fakeSymbolOperator)
     }
 
     @JvmStatic
     fun canCreateSymbolOperator(project: Project, name: String): Boolean {
         val fakeSymbolOperator = "${fakeProgram}($name) :: Int -> Int -> Int"
-        return createElementOrNull(project, fakeSymbolOperator, FregeSymbolOperator::class) != null
+        return createElementOrNull<FregeSymbolOperator>(project, fakeSymbolOperator) != null
     }
 }
