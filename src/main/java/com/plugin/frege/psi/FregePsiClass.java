b78228ac4fa1888ffd8393fd49ccbab0fb75fbed
Petya Surkov <petyo333@gmail.com>
2021-06-06 12:49:26 +0300

    Support documentation for class. Display links from class documentation to methods documentation and vice versa

--- src/main/java/com/plugin/frege/psi/FregePsiClass.java
+++ src/main/java/com/plugin/frege/psi/FregePsiClass.java
@@ -1,6 +1,14 @@
 package com.plugin.frege.psi;
 
 import com.intellij.psi.PsiClass;
+import com.plugin.frege.psi.impl.FregePsiUtilImpl;
+import org.jetbrains.annotations.NotNull;
 
-public interface FregePsiClass extends FregeNamedElement, PsiClass {
+import java.util.List;
+
+public interface FregePsiClass extends FregeNamedElement, PsiClass, FregeElementProvideDocumentation {
+    @Override
+    default @NotNull List<@NotNull FregeDocumentationElement> getDocs() {
+        return FregePsiUtilImpl.collectPrecedingDocs(this);
+    }
 }
--- src/main/kotlin/com/plugin/frege/documentation/FregeGenerateDocUtil.kt
+++ src/main/kotlin/com/plugin/frege/documentation/FregeGenerateDocUtil.kt
@@ -3,24 +3,17 @@ package com.plugin.frege.documentation
 import com.intellij.lang.documentation.DocumentationMarkup.*
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.parentOfType
+import com.plugin.frege.psi.FregeClassDecl
 import com.plugin.frege.psi.FregeDocumentationElement
 import com.plugin.frege.psi.FregeProgram
 import com.plugin.frege.psi.FregePsiMethod
 import com.plugin.frege.psi.impl.FregeAnnotationItemImpl
 import com.plugin.frege.psi.impl.FregeBindingImpl
+import com.plugin.frege.psi.impl.FregeClassDeclImpl
 import com.plugin.frege.psi.impl.FregePsiUtilImpl
 
 object FregeGenerateDocUtil {
-    private fun renderModuleLinkDocFor(element: PsiElement): String {
-        val packageName = element.parentOfType<FregeProgram>()?.packageName ?: return "No module\n"
-        return FregeHtmlPsiUtil.packageNameLink(packageName) + "\n"
-    }
-
-    private fun renderAnnoItem(annoItem: FregeAnnotationItemImpl?): String {
-        val type = annoItem?.getAnnotation()?.sigma?.text ?: return ""
-        return "Annotation: $type\n"
-    }
-
+    @JvmStatic
     fun generateFregeMethodDoc(method: FregePsiMethod): String {
         val annoItem = when (method) {
             is FregeBindingImpl -> method.getAnnoItem() as? FregeAnnotationItemImpl
@@ -30,7 +23,9 @@ object FregeGenerateDocUtil {
         return buildString {
             append(DEFINITION_START)
             append(renderModuleLinkDocFor(method))
-            append(renderAnnoItem(annoItem))
+            append("<h3>Function ${method.name}</h3>")
+            append(renderAnnotationIn(annoItem))
+            append(renderWithin(method))
             append(DEFINITION_END)
             append(CONTENT_START)
             append(renderDocs(annoItem?.getDocs()))
@@ -38,14 +33,54 @@ object FregeGenerateDocUtil {
             for (binding in FregePsiUtilImpl.getAllBindingsOfMethod(method)) {
                 append("<p>")
                 append(renderDocs(binding.getDocs()))
-                append("<h2>" + binding.lhs.text + "</h2>")
+                append("<h4><code>" + binding.lhs.text + "</code></h4>")
                 append("</p>")
             }
             append(CONTENT_END)
         }
     }
 
+    @JvmStatic
+    fun generateFregeClassDoc(fregeClass: FregeClassDeclImpl): String {
+        val uniqueMethods = fregeClass.allMethods.distinctBy { it.name }.mapNotNull { it as? FregePsiMethod }
+        return buildString {
+            append(DEFINITION_START)
+            append(renderModuleLinkDocFor(fregeClass))
+            append("<h3>Class ${fregeClass.name}</h3>")
+            append(DEFINITION_END)
+            append(CONTENT_START)
+            append(renderDocs(fregeClass.getDocs()))
+            append("<h3>Functions:</h3>")
+            for (method in uniqueMethods) {
+                append("<p>")
+                append(FregeHtmlPsiUtil.psiMethodLink(method))
+                append("</p>")
+            }
+            append(CONTENT_END)
+        }
+    }
+
+    @JvmStatic
+    private fun renderModuleLinkDocFor(element: PsiElement): String {
+        val packageName = element.parentOfType<FregeProgram>()?.packageName ?: return "No module"
+        return FregeHtmlPsiUtil.packageNameLink(packageName)
+    }
+
+    @JvmStatic
+    private fun renderAnnotationIn(annoItem: FregeAnnotationItemImpl?): String {
+        val type = annoItem?.getAnnotation()?.sigma?.text ?: return ""
+        return "<h3><i>$type</i></h3>"
+    }
+
+    @JvmStatic
+    private fun renderWithin(method: FregePsiMethod): String {
+        val classDecl = method.containingClass as? FregeClassDecl ?: return ""
+        val classLink = FregeHtmlPsiUtil.psiClassLink(classDecl) ?: return ""
+        return " within $classLink"
+    }
+
+    @JvmStatic
     private fun renderDocs(docs: List<FregeDocumentationElement>?): String {
-        return docs?.joinToString("\n") { doc -> doc.documentationText } ?: ""
+        return docs?.joinToString("<br>") { doc -> doc.documentationText.trim() } ?: ""
     }
 }
