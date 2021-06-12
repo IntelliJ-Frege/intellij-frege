3002c1b8ffd7a3d82d57deb02e6debf404ec1aa6
Kirill <bitikit@gmail.com>
2021-06-12 03:31:32 +0300

    Implement short class name index for frege

--- src/main/kotlin/com/plugin/frege/stubs/types/FregeClassElementType.kt
+++ src/main/kotlin/com/plugin/frege/stubs/types/FregeClassElementType.kt
@@ -1,25 +1,37 @@
 package com.plugin.frege.stubs.types
 
-import com.intellij.psi.stubs.StubElement
-import com.intellij.psi.stubs.StubIndexKey
-import com.intellij.psi.stubs.StubInputStream
+import com.intellij.psi.stubs.*
 import com.plugin.frege.psi.FregePsiClass
+import com.plugin.frege.psi.impl.FregePsiUtilImpl.nameFromQualifiedName
 import com.plugin.frege.stubs.FregeClassStub
 import com.plugin.frege.stubs.index.FregeClassNameIndex
+import com.plugin.frege.stubs.index.FregeShortClassNameIndex
 
 abstract class FregeClassElementType(debugName: String) :
     FregeNamedStubElementType<FregeClassStub, FregePsiClass>(debugName) {
 
-    override val key: StubIndexKey<Int, FregePsiClass>
-        get() = FregeClassNameIndex.INSTANCE.key
+    override val nameKey = FregeClassNameIndex.INSTANCE.key
+
+    protected val shortNameKey = FregeShortClassNameIndex.INSTANCE.key
+
+    override fun indexStub(stub: FregeClassStub, sink: IndexSink) {
+        super.indexStub(stub, sink)
+        val qualifiedName = stub.name ?: return
+        val shortName = nameFromQualifiedName(qualifiedName)
+        sink.occurrence(shortNameKey, shortName.hashCode())
+    }
 
     override fun createStub(psi: FregePsiClass, parentStub: StubElement<*>?): FregeClassStub {
         return FregeClassStub(parentStub, this, psi.qualifiedName)
     }
 
+    override fun serialize(stub: FregeClassStub, dataStream: StubOutputStream) {
+        dataStream.writeName(stub.name)
+    }
+
     override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>): FregeClassStub {
-        val name = dataStream.readName()
-        return FregeClassStub(parentStub, this, name)
+        val qualifiedName = dataStream.readName()
+        return FregeClassStub(parentStub, this, qualifiedName)
     }
 
     override fun getExternalId(): String {
--- src/main/kotlin/com/plugin/frege/stubs/types/FregeMethodElementType.kt
+++ src/main/kotlin/com/plugin/frege/stubs/types/FregeMethodElementType.kt
@@ -1,8 +1,8 @@
 package com.plugin.frege.stubs.types
 
 import com.intellij.psi.stubs.StubElement
-import com.intellij.psi.stubs.StubIndexKey
 import com.intellij.psi.stubs.StubInputStream
+import com.intellij.psi.stubs.StubOutputStream
 import com.plugin.frege.psi.FregePsiMethod
 import com.plugin.frege.stubs.FregeMethodStub
 import com.plugin.frege.stubs.index.FregeMethodNameIndex
@@ -10,11 +10,14 @@ import com.plugin.frege.stubs.index.FregeMethodNameIndex
 abstract class FregeMethodElementType(debugName: String) :
     FregeNamedStubElementType<FregeMethodStub, FregePsiMethod>(debugName) {
 
-    override val key: StubIndexKey<Int, FregePsiMethod>
-        get() = FregeMethodNameIndex.INSTANCE.key
+    override val nameKey = FregeMethodNameIndex.INSTANCE.key
 
     override fun createStub(psi: FregePsiMethod, parentStub: StubElement<*>?): FregeMethodStub {
         return FregeMethodStub(parentStub, this, psi.name)
+    }
+
+    override fun serialize(stub: FregeMethodStub, dataStream: StubOutputStream) {
+        dataStream.writeName(stub.name)
     }
 
     override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>): FregeMethodStub {
