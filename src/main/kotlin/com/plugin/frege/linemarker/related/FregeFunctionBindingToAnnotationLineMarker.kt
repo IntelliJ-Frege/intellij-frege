93bf28b1622afb4ff29fd6f6d90b58a3b2ca2f23
Petya Surkov <petyo333@gmail.com>
2021-06-15 14:05:44 +0300

    Migrate related line markers to Kotlin

---
+++ src/main/kotlin/com/plugin/frege/linemarker/related/FregeFunctionBindingToAnnotationLineMarker.kt
@@ -0,0 +1,21 @@
+package com.plugin.frege.linemarker.related
+
+import com.intellij.icons.AllIcons
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.plugin.frege.psi.impl.FregeBindingImpl
+import com.plugin.frege.psi.impl.FregeFunctionNameImpl
+import javax.swing.Icon
+
+class FregeFunctionBindingToAnnotationLineMarker : FregeRelatedItemLineMarkerAbstract() {
+    override fun getTargets(element: PsiElement): List<PsiElement> {
+        val parent = element.parent as? FregeFunctionNameImpl ?: return emptyList()
+        val binding = PsiTreeUtil.getParentOfType(parent, FregeBindingImpl::class.java) ?: return emptyList()
+        return listOfNotNull(binding.getAnnoItem())
+    }
+
+    override val icon: Icon
+        get() = AllIcons.Gutter.ImplementingFunctionalInterface
+    override val tooltipText: String
+        get() = "Navigate to type annotation"
+}
