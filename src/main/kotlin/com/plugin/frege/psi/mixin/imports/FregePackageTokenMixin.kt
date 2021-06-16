e773c9399980f3aee351a9a0570363a360bf2945
Kirill <bitikit@gmail.com>
2021-06-17 02:03:34 +0300

    Implement resolving to short modules names with 'frege' prefix

--- src/main/kotlin/com/plugin/frege/psi/mixin/imports/FregePackageTokenMixin.kt
+++ src/main/kotlin/com/plugin/frege/psi/mixin/imports/FregePackageTokenMixin.kt
@@ -5,9 +5,11 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
+import com.intellij.util.containers.addIfNotNull
 import com.plugin.frege.psi.FregeElementFactory
 import com.plugin.frege.psi.FregePackagePrefix
 import com.plugin.frege.psi.impl.FregeCompositeElementImpl
+import com.plugin.frege.psi.impl.FregePsiUtilImpl
 import com.plugin.frege.resolve.FregeReferenceBase
 
 open class FregePackageTokenMixin(node: ASTNode) : FregeCompositeElementImpl(node) { // TODO implement psi package
@@ -17,8 +19,17 @@ open class FregePackageTokenMixin(node: ASTNode) : FregeCompositeElementImpl(nod
                 val packagePrefix = psiElement.parent as? FregePackagePrefix ?: return emptyList()
                 val packageText = packagePrefix.text
                 val qualifiedName = packageText.substring(0, psiElement.textRange.endOffset - parent.textOffset)
-                val resolvedPackage = JavaPsiFacade.getInstance(psiElement.project).findPackage(qualifiedName)
-                return if (resolvedPackage != null) listOf(resolvedPackage) else emptyList()
+                val results = mutableListOf<PsiElement>()
+                val psiFacade = JavaPsiFacade.getInstance(psiElement.project)
+                results.addIfNotNull(psiFacade.findPackage(qualifiedName))
+
+                if (results.isEmpty()) {
+                    val libraryPackage = FregePsiUtilImpl.tryConvertToLibraryPackage(qualifiedName)
+                    if (libraryPackage != null) {
+                        results.addIfNotNull(psiFacade.findPackage(libraryPackage))
+                    }
+                }
+                return results
             }
 
             override fun handleElementRename(name: String): PsiElement {
