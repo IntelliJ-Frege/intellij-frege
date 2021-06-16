10aa17e2a8a522ca393ada3d97a3cae802346790
Худяков Юрий <catchitoo@mail.ru>
2021-06-16 22:37:08 +0300

    Fix FregeRunAction and FregeReplState to launch run configuration and REPL process correctly

--- src/main/java/com/plugin/frege/repl/FregeReplState.java
+++ src/main/java/com/plugin/frege/repl/FregeReplState.java
@@ -1,11 +1,12 @@
 package com.plugin.frege.repl;
 
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.CommandLineState;
 import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.configurations.JavaCommandLineState;
+import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.filters.Filter;
 import com.intellij.execution.filters.TextConsoleBuilder;
-import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.process.ProcessTerminatedListener;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.ui.ConsoleView;
@@ -21,12 +22,14 @@ import org.jetbrains.annotations.NotNull;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
-public class FregeReplState extends CommandLineState {
+public class FregeReplState extends JavaCommandLineState {
     private final FregeReplRunConfiguration configuration;
+    private final ExecutionEnvironment environment;
 
     public FregeReplState(FregeReplRunConfiguration configuration, ExecutionEnvironment environment) {
         super(environment);
         this.configuration = configuration;
+        this.environment = environment;
 
         String moduleName = configuration.getSelectedModuleName();
         if (moduleName == null) {
@@ -54,8 +57,22 @@ public class FregeReplState extends CommandLineState {
 
     // TODO exit if no repl in compiler
     @Override
-    protected @NotNull ProcessHandler startProcess() throws ExecutionException {
+    protected @NotNull OSProcessHandler startProcess() throws ExecutionException {
+        GeneralCommandLine commandLine = createCommandLine();
+
+        if (!configuration.getAdditionalArguments().equals(""))
+            commandLine.addParameters(configuration.getAdditionalArguments().split(" "));
+        FregeReplProcessHandler handler = new FregeReplProcessHandler(commandLine.createProcess(), commandLine.getCommandLineString(), StandardCharsets.UTF_8, 1);
+
+        ProcessTerminatedListener.attach(handler);
+        return handler;
+    }
+
+    @Override
+    protected JavaParameters createJavaParameters() throws ExecutionException {
         if (configuration.getSelectedModuleName() == null) throw new ExecutionException("No module selected");
+
+        JavaParameters javaParameters = new JavaParameters();
         Module module = ModuleManager.getInstance(configuration.getProject()).findModuleByName(configuration.getSelectedModuleName());
         String compilerJar;
         try {
@@ -63,30 +80,12 @@ public class FregeReplState extends CommandLineState {
         } catch (GradleFregeException e) {
             throw new ExecutionException(e);
         }
-
+        javaParameters.setJarPath(compilerJar);
         Sdk projectSdk = ProjectRootManager.getInstance(configuration.getProject()).getProjectSdk();
-        String pathToJavaExecutable = null;
         if (projectSdk != null) {
-            String sdkHomePath = projectSdk.getHomePath();
-            if (sdkHomePath != null) {
-                Path possiblePathToJavaExecutable = Path.of(sdkHomePath).resolve("bin/java");
-                if (possiblePathToJavaExecutable.toFile().exists())
-                    pathToJavaExecutable = possiblePathToJavaExecutable.toString();
-            }
+            javaParameters.setJdk(projectSdk);
         }
-        if (pathToJavaExecutable == null) {
-            pathToJavaExecutable = "java"; // system default
-        }
-
-        GeneralCommandLine commandLine = new GeneralCommandLine().
-                withExePath(pathToJavaExecutable).
-                withWorkDirectory(configuration.getProject().getBasePath()).
-                withParameters("-jar", compilerJar);
-        if (!configuration.getAdditionalArguments().equals(""))
-            commandLine.addParameters(configuration.getAdditionalArguments().split(" "));
-        FregeReplProcessHandler handler = new FregeReplProcessHandler(commandLine.createProcess(), commandLine.getCommandLineString(), StandardCharsets.UTF_8, 1);
-
-        ProcessTerminatedListener.attach(handler);
-        return handler;
+        javaParameters.setWorkingDirectory(configuration.getProject().getBasePath());
+        return javaParameters;
     }
 }
