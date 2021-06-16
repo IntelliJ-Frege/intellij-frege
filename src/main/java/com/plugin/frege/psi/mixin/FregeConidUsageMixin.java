005e3831fd16971be187cb3ce4752d59c8d161d6
Kirill <bitikit@gmail.com>
2021-06-16 19:51:20 +0300

    Introduce frege resolvable element

--- src/main/java/com/plugin/frege/psi/mixin/FregeConidUsageMixin.java
+++ src/main/java/com/plugin/frege/psi/mixin/FregeConidUsageMixin.java
@@ -4,12 +4,13 @@ import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiIdentifier;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.tree.IElementType;
+import com.plugin.frege.psi.FregeResolvableElement;
 import com.plugin.frege.psi.FregeTypes;
 import com.plugin.frege.psi.impl.FregeCompositeElementImpl;
 import com.plugin.frege.resolve.FregeConidUsageReference;
 import org.jetbrains.annotations.NotNull;
 
-public class FregeConidUsageMixin extends FregeCompositeElementImpl implements PsiIdentifier {
+public class FregeConidUsageMixin extends FregeCompositeElementImpl implements FregeResolvableElement, PsiIdentifier {
     public FregeConidUsageMixin(@NotNull ASTNode node) {
         super(node);
     }
