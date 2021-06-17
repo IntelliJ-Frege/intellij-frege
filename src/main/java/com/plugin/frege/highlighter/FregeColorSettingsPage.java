ad65fc3f59e799a6b9f4dd79095e3a58d8d289eb
Kirill <bitikit@gmail.com>
2021-06-17 21:22:43 +0300

    Add docs in color settings page and update demo text for color preview

--- src/main/java/com/plugin/frege/highlighter/FregeColorSettingsPage.java
+++ src/main/java/com/plugin/frege/highlighter/FregeColorSettingsPage.java
@@ -1,5 +1,6 @@
 package com.plugin.frege.highlighter;
 
+import com.google.common.collect.ImmutableMap;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.options.colors.AttributesDescriptor;
@@ -26,9 +27,17 @@ public class FregeColorSettingsPage implements ColorSettingsPage {
             new AttributesDescriptor("Function name", FregeSyntaxHighlighter.FUNCTION_NAME),
             new AttributesDescriptor("Line comment", FregeSyntaxHighlighter.LINE_COMMENT),
             new AttributesDescriptor("Block comment", FregeSyntaxHighlighter.BLOCK_COMMENT),
+            new AttributesDescriptor("Line documentation", FregeSyntaxHighlighter.LINE_DOC),
+            new AttributesDescriptor("Block documentation", FregeSyntaxHighlighter.BLOCK_DOC),
             new AttributesDescriptor("Undefined", FregeSyntaxHighlighter.UNDEFINED),
             new AttributesDescriptor("Bad value", FregeSyntaxHighlighter.BAD_CHARACTER)
     };
+
+    private static final Map<String, TextAttributesKey> additionalHighlightingTagToDescriptorMap = ImmutableMap.of(
+            "funcName", FregeSyntaxHighlighter.FUNCTION_NAME,
+            "import", FregeSyntaxHighlighter.TYPE,
+            "undefined", FregeSyntaxHighlighter.UNDEFINED
+    );
 
     @Nullable
     @Override
@@ -47,40 +56,43 @@ public class FregeColorSettingsPage implements ColorSettingsPage {
     public String getDemoText() {
         return "-- https://github.com/Frege/frege/blob/master/examples/Grep.fr" +
                 "--- A simple grep\n" +
-                "module examples.Grep where\n" +
+                "module <import>examples.Grep</import> where\n" +
+                "\n" +
+                "import <import>another.mine.Hello</import> -- importing a module" +
+                "\n" +
                 "\n" +
                 "--- exception thrown when an invalid regular expression is compiled\n" +
                 "data PatternSyntax = native java.util.regex.PatternSyntaxException\n" +
                 "derive Exceptional PatternSyntax\n" +
                 "\n" +
-                "main [] = stderr.println \"Usage: java examples.Grep regex [files ...]\"\n" +
-                "main (pat:xs) = do\n" +
+                "<funcName>main</funcName> [] = stderr.println \"Usage: java examples.Grep regex [files ...]\"\n" +
+                "<funcName>main</funcName> (pat:xs) = do\n" +
                 "        rgx <- return (regforce pat)\n" +
                 "        case xs of\n" +
                 "            [] -> grepit rgx stdin\n" +
                 "            fs -> mapM_ (run rgx) fs\n" +
                 "     `catch` badpat where\n" +
-                "        badpat :: PatternSyntax -> IO ()\n" +
-                "        badpat pse = do\n" +
+                "        <funcName>badpat</funcName> :: PatternSyntax -> IO ()\n" +
+                "        <funcName>badpat</funcName> pse = do\n" +
                 "            stderr.println \"The regex is not valid.\"\n" +
                 "            stderr.println pse.getMessage        \n" +
                 "\n" +
-                "notImplemented = undefined" +
+                "<funcName>notImplemented</funcName> = <undefined>undefined</undefined>" +
                 "\n" +
-                "run regex file = do\n" +
+                "<funcName>run</funcName> regex file = do\n" +
                 "        rdr <- openReader file\n" +
                 "        grepit regex rdr\n" +
                 "    `catch` fnf where\n" +
-                "        fnf :: FileNotFoundException -> IO ()\n" +
-                "        fnf _ = stderr.println (\"Could not read \" ++ file)\n" +
+                "        <funcName>fnf</funcName> :: FileNotFoundException -> IO ()\n" +
+                "        <funcName>fnf</funcName> _ = stderr.println (\"Could not read \" ++ file)\n" +
                 "\n" +
                 "\n" +
-                "grepit :: Regex -> MutableIO BufferedReader -> IO ()                \n" +
-                "grepit pat rdr = forever line `catch` eof `finally` rdr.close \n" +
+                "<funcName>grepit</funcName> :: Regex -> MutableIO BufferedReader -> IO ()                \n" +
+                "<funcName>grepit</funcName> pat rdr = forever line `catch` eof `finally` rdr.close \n" +
                 "    where\n" +
-                "        eof :: EOFException -> IO ()\n" +
-                "        eof _ = return ()\n" +
-                "        line = do\n" +
+                "        <funcName>eof</funcName> :: EOFException -> IO ()\n" +
+                "        <funcName>eof</funcName> _ = return ()\n" +
+                "        <funcName>line</funcName> = do\n" +
                 "            line <- rdr.getLine \n" +
                 "            when (line ~ pat) (println line)";
     }
@@ -88,7 +100,7 @@ public class FregeColorSettingsPage implements ColorSettingsPage {
     @Nullable
     @Override
     public Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
-        return null;
+        return additionalHighlightingTagToDescriptorMap;
     }
 
     @Override
