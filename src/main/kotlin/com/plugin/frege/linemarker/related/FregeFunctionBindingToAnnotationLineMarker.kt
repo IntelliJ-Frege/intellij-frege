b876294da97424bee5af1cd783d1f6b195dd26db
Kirill Karnaukhov <bitikit@gmail.com>
2021-08-17 02:11:22 +0300

    Fix line markers for type annotations of operators (issue #102)

--- src/main/kotlin/com/plugin/frege/linemarker/related/FregeFunctionBindingToAnnotationLineMarker.kt
+++ src/main/kotlin/com/plugin/frege/linemarker/related/FregeFunctionBindingToAnnotationLineMarker.kt
@@ -3,19 +3,23 @@ package com.plugin.frege.linemarker.related
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.plugin.frege.FregeIcons
+import com.plugin.frege.psi.FregeFunctionLhs
 import com.plugin.frege.psi.impl.FregeBindingImpl
-import com.plugin.frege.psi.impl.FregeFunctionNameImpl
 import javax.swing.Icon
 
 class FregeFunctionBindingToAnnotationLineMarker : FregeRelatedItemLineMarkerAbstract() {
+    override val icon get(): Icon = FregeIcons.AnnotationMarker
+
+    override val tooltipText get(): String = "Navigate to type annotation"
+
     override fun getTargets(element: PsiElement): List<PsiElement> {
-        val parent = element.parent as? FregeFunctionNameImpl ?: return emptyList()
+        val parent = tryGetFunctionLhs(element) ?: return emptyList()
         val binding = PsiTreeUtil.getParentOfType(parent, FregeBindingImpl::class.java) ?: return emptyList()
-        return listOfNotNull(binding.getAnnoItem())
+        return if (binding.nameIdentifier === element) listOfNotNull(binding.getAnnoItem()) else emptyList()
     }
 
-    override val icon: Icon
-        get() = FregeIcons.AnnotationMarker
-    override val tooltipText: String
-        get() = "Navigate to type annotation"
+    private fun tryGetFunctionLhs(element: PsiElement): FregeFunctionLhs? {
+        return generateSequence(element.parent) { it.parent }.take(3)
+            .filterIsInstance<FregeFunctionLhs>().firstOrNull()
+    }
 }
