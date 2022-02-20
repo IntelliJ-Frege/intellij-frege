0663d7dff9213a7585d80743676b2864946f947b
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2022-02-20 17:50:38 +0300

    Fix completion for keywords (issue #118)

--- src/main/kotlin/com/plugin/frege/completion/FregeCompletionUtil.kt
+++ src/main/kotlin/com/plugin/frege/completion/FregeCompletionUtil.kt
@@ -7,7 +7,6 @@ object FregeCompletionUtil {
     @JvmStatic
     fun shouldComplete(element: PsiElement): Boolean {
         // There are problems with unnecessary completion after numbers/etc
-        // The first condition for optimization
-        return element.text == CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED
+        return element.text != CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED
     }
 }
